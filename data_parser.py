e016a25e7ade5180a8b06347eedb9364ce214cfb
kcmodev <shchristensen3@gmail.com>
2020-11-24 16:53:12 -0600

    working on package data structure

--- data_parser.py
+++ data_parser.py
@@ -1,26 +1,51 @@
 import csv
 
+class Package:
+
+    def __init__(self, package_id, address, city, state, zip_code,
+                               delivery_deadline, mass, special_notes):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.delivery_deadline = delivery_deadline
+        self.mass = mass
+        self.special_notes = special_notes
+
+# class TruckStop:
+#
+#     def __init__(self):
+
 
 def parse_distance_table():
-    print()
-    print('*' * 100)
+    print('Distance table loaded and parsed.')
     with open('data/Distance Table.csv') as distance_table:
-        lines = csv.reader(distance_table, delimiter=',')
+        lines = csv.reader(distance_table)
 
         for x, line in enumerate(lines):
-            print(f'Line #{x}: {line}')
-
-    print('*' * 150)
+            # print(f'Line #{x}: {line}')
+            pass
 
 
 def parse_package_file():
-    print()
-    print('*' * 150)
+    packages = []
 
+    print('Package file loaded and parsed.')
     with open('data/Package File.csv') as distance_table:
-        lines = csv.reader(distance_table, delimiter=',')
+        lines = csv.reader(distance_table)
 
         for x, line in enumerate(lines):
-            print(f'Line #{x}: {line}')
+            packages.append([line[0], line[1], line[2], line[3], line[4],
+                             line[5], line[6]])
 
-    print('*' * 100)
+    for package in packages:
+        print(package)
+
+
+def package_search(package_id: int):
+    print(f'Package ID #{package_id} information:\n')
+
+
+def print_line_break():
+    print('*' * 200)
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import data_parser as d_p
 
-d_p.parse_distance_table()
-d_p.parse_package_file()
+
+def main():
+    d_p.parse_distance_table()
+    # d_p.print_line_break()
+    d_p.parse_package_file()
+
+    while True:
+        billboard()
+
+
+def billboard():
+    print("""
+            Please make a selection:
+            1. Search by package ID
+    """)
+
+    user_selection = int(input('Selection: '))
+
+    if user_selection == 1:
+        package = int(input('Enter the Package ID: '))
+        d_p.package_search(package)
+    else:
+        print('Invalid choice.')
+        billboard()
+
+
+if __name__ == '__main__':
+    main()
