87e3765710be9438ed7a397ad061507489399ea1
kcmodev <shchristensen3@gmail.com>
2020-11-24 18:45:32 -0600

    implemented basic hash table

--- data_parser.py
+++ data_parser.py
@@ -1,9 +1,12 @@
 import csv
 
+packages = []
+
+
 class Package:
 
     def __init__(self, package_id, address, city, state, zip_code,
-                               delivery_deadline, mass, special_notes):
+                 delivery_deadline, mass, special_notes):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -12,6 +15,7 @@ class Package:
         self.delivery_deadline = delivery_deadline
         self.mass = mass
         self.special_notes = special_notes
+
 
 # class TruckStop:
 #
@@ -29,23 +33,57 @@ def parse_distance_table():
 
 
 def parse_package_file():
-    packages = []
+    """
+    Parses csv file `Package File` and fills a list with the data. Uses
+    `package id` as the index for the list to make searching O(1)
+    :return: None. Fills list with imported data.
+    """
+    global packages
 
     print('Package file loaded and parsed.')
     with open('data/Package File.csv') as distance_table:
         lines = csv.reader(distance_table)
 
         for x, line in enumerate(lines):
-            packages.append([line[0], line[1], line[2], line[3], line[4],
-                             line[5], line[6]])
+            if x != 0:
+                package_id = int(line[0])
+                address = line[1]
+                city = line[2]
+                state = line[3]
+                zip_code = line[4]
+                deadline = line[5]
+                mass = line[6]
 
-    for package in packages:
-        print(package)
+                if line[7] is not None:
+                    note = line[7]
+                else:
+                    note = None
+
+                # package = Package(package_id, address, city, state,
+                #                   zip_code, deadline, mass, note)
+                #
+                # packages.append([getattr(package, 'package_id'), [
+                #     getattr(package, 'address'),
+                #     getattr(package, 'city'),
+                #     getattr(package, 'state'),
+                #     getattr(package, 'zip_code'),
+                #     getattr(package, 'delivery_deadline'),
+                #     getattr(package, 'mass'),
+                #     getattr(package, 'special_notes')
+                # ]])
+                packages.append([package_id, [address, city, state, zip_code,
+                                              deadline, mass, note]])
+
+    # for package in packages:
+    #     print(package)
 
 
-def package_search(package_id: int):
+def package_search(package_id: int) -> object:
+    print_line_break()
+    print()
     print(f'Package ID #{package_id} information:\n')
-
+    print(packages[package_id - 1])
+    print()
 
 def print_line_break():
-    print('*' * 200)
+    print('*' * 100)
--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import data_parser as d_p
 
 
 def main():
+    print()
     d_p.parse_distance_table()
     # d_p.print_line_break()
     d_p.parse_package_file()
+    print()
 
     while True:
         billboard()
 
 
 def billboard():
+    d_p.print_line_break()
     print("""
             Please make a selection:
             1. Search by package ID
     """)
+    d_p.print_line_break()
 
     user_selection = int(input('Selection: '))
 
